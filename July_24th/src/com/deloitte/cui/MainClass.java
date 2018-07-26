39cc58d3b81f3656d9c9a906f45f28741c528f4f
sadhanak27 <sadhanak27@users.noreply.github.com>
2018-07-26 08:58:38 +0530

    Second commit

--- July_24th/src/com/deloitte/cui/MainClass.java
+++ July_24th/src/com/deloitte/cui/MainClass.java
@@ -1,10 +1,34 @@
 package com.deloitte.cui;
 
+class Point{
+	
+	public int x;
+	public int y;
+}
+
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		Point[] points;
+		points = new Point[3];
+		
+		points[0] = new Point();
+		points[1] = new Point();
+		points[2] = new Point();
+		
+		points[0].x = 1;
+		points[0].y = 2;
+		points[1].x = 3;
+		points[1].y = 4;
+		points[2].x = 5;
+		points[2].y = 6;
+		
+      for(Point point:points) {
+    	  System.out.println(point.x+" "+point.y);
+      }
+    	  
 	}
 
 }
